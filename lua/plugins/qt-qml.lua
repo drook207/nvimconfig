19b3fcc8ac4325a295bc37501f301fc4d253fed8
Pascal Berndt <drook207@gmail.com>
2025-10-12 21:28:31 +0200

    Add keymaps for cmake plugin

--- lua/plugins/qt-qml.lua
+++ lua/plugins/qt-qml.lua
@@ -1,44 +1,45 @@
 return {
-  -- QML Language Server Support
-  {
-    "neovim/nvim-lspconfig",
-    opts = {
-      servers = {
-        qmlls = {
-          cmd = { "qmlls" },
-          filetypes = { "qml" },
-          root_dir = function(fname)
-            return require("lspconfig.util").find_git_ancestor(fname) or vim.fn.getcwd()
-          end,
-          settings = {},
-        },
-      },
-    },
-  },
+	-- QML Language Server Support
+	{
+		"neovim/nvim-lspconfig",
+		opts = {
+			servers = {
+				qmlls = {
+					cmd = { "qmlls" },
+					filetypes = { "qml" },
+					root_dir = function(fname)
+						return require("lspconfig.util").find_git_ancestor(fname) or vim.fn.getcwd()
+					end,
+					settings = {},
+				},
+			},
+		},
+	},
 
-  -- QML Treesitter Support
-  {
-    "nvim-treesitter/nvim-treesitter",
-    opts = function(_, opts)
-      vim.list_extend(opts.ensure_installed, {
-        "qmljs",
-      })
-    end,
-  },
+	-- QML Treesitter Support
+	{
+		"nvim-treesitter/nvim-treesitter",
+		opts = function(_, opts)
+			vim.list_extend(opts.ensure_installed, {
+				"qmljs",
+			})
+		end,
+	},
 
-  -- Mason for automatic LSP installation
-  {
-    "mason-org/mason.nvim",
-    opts = {
-      ensure_installed = {
-        "qmlls",
-      },
-    },
-  },
+	-- Mason for automatic LSP installation
+	{
+		"mason-org/mason.nvim",
+		opts = {
+			ensure_installed = {
+				"qmlls",
+			},
+		},
+	},
 
-  -- QML file type detection and syntax highlighting
-  {
-    "peterhoeg/vim-qml",
-    ft = { "qml" },
-  },
+	-- QML file type detection and syntax highlighting
+	{
+		"peterhoeg/vim-qml",
+		ft = { "qml" },
+	},
 }
+
